a87a2f4c42116b067ffa8d6812da0efbb745f453
Nikos Naoum <nikosnaoumv2@outlook.com>
2021-06-21 00:31:45 +0300

    Fixed a runtimer error where the total amount of ram would not get converted at all to GB.

--- WindowsOptimizations.Core/Patches/CPUProcessPatch.cs
+++ WindowsOptimizations.Core/Patches/CPUProcessPatch.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using System;
 using System.Linq;
 using System.Management;
 using System.Windows;
@@ -20,50 +21,50 @@ namespace WindowsOptimizations.Core.Patches
         {
             // Get total amount of RAM installed.
             string query = "SELECT Capacity FROM Win32_PhysicalMemory";
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+            ManagementObjectSearcher searcher = new(query);
 
-            long result = searcher
+            string totalRamAmount = StringExtensions.ToSize(searcher
                 .Get()
                 .Cast<ManagementObject>()
-                .Sum(x => (long)x.Properties["Capacity"].Value);
+                .Sum(x => Convert.ToInt64(x.Properties["Capacity"].Value)), SizeUnits.GB);
 
             // Set the svc host splitting threshold accoring to the total amount of ram.
-            switch (IntExtensions.ToSize(result, SizeUnits.GB))
+            switch (totalRamAmount)
             {
-                case "4":
+                case "4.00":
                     Registry.SetValue(RegistryKeys.CurrentControlKey, "SvcHostSplitThresholdInKB", 4194304);
                     break;
 
-                case "6":
+                case "6.00":
                     Registry.SetValue(RegistryKeys.CurrentControlKey, "SvcHostSplitThresholdInKB", 6291456);
                     break;
 
-                case "8":
+                case "8.00":
                     Registry.SetValue(RegistryKeys.CurrentControlKey, "SvcHostSplitThresholdInKB", 8388608);
                     break;
 
-                case "12":
+                case "12.00":
                     Registry.SetValue(RegistryKeys.CurrentControlKey, "SvcHostSplitThresholdInKB", 12582912);
                     break;
 
-                case "16":
+                case "16.00":
                     Registry.SetValue(RegistryKeys.CurrentControlKey, "SvcHostSplitThresholdInKB", 16777216);
                     break;
 
-                case "24":
+                case "24.00":
                     Registry.SetValue(RegistryKeys.CurrentControlKey, "SvcHostSplitThresholdInKB", 25165824);
                     break;
 
-                case "32":
+                case "32.00":
                     Registry.SetValue(RegistryKeys.CurrentControlKey, "SvcHostSplitThresholdInKB", 33554432);
                     break;
 
-                case "64":
+                case "64.00":
                     Registry.SetValue(RegistryKeys.CurrentControlKey, "SvcHostSplitThresholdInKB", 67108864);
                     break;
 
                 default:
-                    MessageBox.Show("Your total amount of RAM is either lower than 4GB or bigger than 64GB. This optimization cannot be applied because of that.", nameof(CPUProcessPatch), MessageBoxButton.OK, MessageBoxImage.Error);
+                    MessageBox.Show("Your total amount of RAM is either lower than 4GB or bigger than 64GB. This optimization cannot be applied because of that." + totalRamAmount, nameof(CPUProcessPatch), MessageBoxButton.OK, MessageBoxImage.Error);
                     break;
             }
 
