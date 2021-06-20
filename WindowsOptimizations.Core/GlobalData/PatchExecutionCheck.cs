25b324159b793a04a36fe3c2347ee69b477728ce
Nikos Naoum <nikosnaoumv2@outlook.com>
2021-06-21 00:30:23 +0300

    Added a new check for the new patch.

--- WindowsOptimizations.Core/GlobalData/PatchExecutionCheck.cs
+++ WindowsOptimizations.Core/GlobalData/PatchExecutionCheck.cs
@@ -6,28 +6,33 @@
     public struct PatchExecutionCheck
     {
         /// <summary>
-        /// Contains a value wether the user has disabled unnecessary windows services by using this application.
+        /// Contains a value whether or not the user has disabled unnecessary windows services by using this application.
         /// </summary>
         public static bool HasDisabledUnnecessaryWindowsServices { get; set; }
 
         /// <summary>
-        /// Contains a value wether the user has reduced mouse input latency by using this application.
+        /// Contains a value whether or not the user has reduced mouse input latency by using this application.
         /// </summary>
         public static bool HasReducedMouseInputLatency { get; set; }
 
         /// <summary>
-        /// Contains a value wether the user has optimized the system profile in this kernel by using this application.
+        /// Contains a value whether or not the user has optimized the system profile in this kernel by using this application.
         /// </summary>
         public static bool HasOptimizedSystemProfile { get; set; }
 
         /// <summary>
-        /// Contains a value wether the user has debloated windows by using Windows10Debloater and Sophia Script through this application.
+        /// Contains a value whether or not the user has debloated windows by using Windows10Debloater and Sophia Script through this application.
         /// </summary>
         public static bool HasDebloatedWindows { get; set; }
 
         /// <summary>
-        /// Contains a value wether or the user has optimized the way windows manages TCP/UDP.
+        /// Contains a value whether or not the user has optimized the way windows manages network options by using this application.
         /// </summary>
         public static bool HasOptimizedNetworkOptions { get; set; }
+
+        /// <summary>
+        /// Contains a value whether or not the user has optimized the way CPU processes are handled and reduced by using this application.
+        /// </summary>
+        public static bool HasReducedCPUProcesses { get; set; }
     }
 }
