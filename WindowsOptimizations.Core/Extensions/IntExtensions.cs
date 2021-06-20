8117e3efcfea28b1576468c3e3edfa941be71db9
Nikos Naoum <nikosnaoumv2@outlook.com>
2021-06-20 23:58:08 +0300

    Added an extension class for converting Int64 values to human readable computer storage size values (MB, GB, etc).

---
+++ WindowsOptimizations.Core/Extensions/IntExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// Source: https://stackoverflow.com/questions/14488796/does-net-provide-an-easy-way-convert-bytes-to-kb-mb-gb-etc
+namespace WindowsOptimizations.Core.Extensions
+{
+    public enum SizeUnits
+    {
+        Byte, KB, MB, GB,
+    }
+
+    public class IntExtensions
+    {
+        public static string ToSize(long value, SizeUnits unit)
+        {
+            return (value / (double)Math.Pow(1024, (long)unit)).ToString("0.00");
+        }
+    }
+}
