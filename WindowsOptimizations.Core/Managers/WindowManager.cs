9112639f1568973dfb03da5e83b0516842185c88
Nikos Naoum <nikosnaoumv2@outlook.com>
2021-07-06 21:25:52 +0300

    Created a manager for showing other views.

---
+++ WindowsOptimizations.Core/Managers/WindowManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace WindowsOptimizations.Core.Managers
+{
+    /// <summary>
+    /// Manages the way views are handled.
+    /// </summary>
+    public class WindowManager
+    {
+        /// <summary>
+        /// Opens a new window with the specified view and viewmodel as data context while waiting for it to exit.
+        /// </summary>
+        /// <typeparam name="TView">The specified view.</typeparam>
+        /// <typeparam name="TViewModel">The specified viewmodel.</typeparam>
+        public static void ShowBlockingView<TView, TViewModel>()
+            where TView : Window, new()
+            where TViewModel : class, new()
+        {
+            Window view = new TView()
+            {
+                DataContext = new TViewModel(),
+            };
+
+            view.ShowDialog();
+        }
+    }
+}
