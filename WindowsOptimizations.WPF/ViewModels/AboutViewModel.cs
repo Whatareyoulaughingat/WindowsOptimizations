f42f1dec54c117a08dc849bc859e2cf47d7f189d
Nikos Naoum <nikosnaoumv2@outlook.com>
2021-07-06 21:26:34 +0300

    Improved the 'About' information.

---
+++ WindowsOptimizations.WPF/ViewModels/AboutViewModel.cs
@@ -0,0 +1,30 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace WindowsOptimizations.WPF.ViewModels
+{
+    /// <summary>
+    /// The viewmodel of <see cref="Views.About"/>.
+    /// </summary>
+    public class AboutViewModel : ReactiveObject
+    {
+        [Reactive]
+        public string ApplicationLicenseInfo { get; set; }
+
+        [Reactive]
+        public string DisclaimerInfo { get; set; }
+
+        [Reactive]
+        public string AcknowledgementsInfo { get; set; }
+
+        public AboutViewModel()
+        {
+            ApplicationLicenseInfo = "This application is licensed under the MIT license.";
+
+            DisclaimerInfo = "This application is not affliated with Windows10Debloater or SophiaScript or any other application.";
+
+            AcknowledgementsInfo = "Windows10Debloater: https://github.com/Sycnex/Windows10Debloater \n" +
+                                   "SophiaScript: https://github.com/farag2/Windows-10-Sophia-Script";
+        }
+    }
+}
---
+++ WindowsOptimizations.WPF/Views/About.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Reactive.Disposables;
+using ReactiveUI;
+using WindowsOptimizations.WPF.ViewModels;
+
+namespace WindowsOptimizations.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for AboutView.xaml.
+    /// </summary>
+    public partial class About : ReactiveWindow<AboutViewModel>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="About"/> class.
+        /// </summary>
+        public About()
+        {
+            InitializeComponent();
+            ViewModel = new AboutViewModel();
+
+            this.WhenActivated(disposableRegistration =>
+            {
+                this.OneWayBind(
+                    ViewModel,
+                    vmProperty => vmProperty.ApplicationLicenseInfo,
+                    viewProperty => viewProperty.License.Content)
+                .DisposeWith(disposableRegistration);
+
+                this.OneWayBind(
+                    ViewModel,
+                    vmProperty => vmProperty.DisclaimerInfo,
+                    viewProperty => viewProperty.Disclaimer.Content)
+                .DisposeWith(disposableRegistration);
+
+                this.OneWayBind(
+                    ViewModel,
+                    vmProperty => vmProperty.AcknowledgementsInfo,
+                    viewProperty => viewProperty.Acknowledgements.Content)
+                .DisposeWith(disposableRegistration);
+            });
+        }
+    }
+}
