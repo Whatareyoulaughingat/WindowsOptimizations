2ca6d998211ebf622be0b04b6d651c8a24ac3ae1
Nikos Naoum <nikosnaoumv2@outlook.com>
2021-08-29 13:41:39 +0300

    Removed OnExit and async voids.

--- VariousWindowsTweaks.WPF/App.xaml.cs
+++ VariousWindowsTweaks.WPF/App.xaml.cs
@@ -1,11 +1,9 @@
-﻿using System.Diagnostics;
-using System.IO;
+﻿using System.IO;
 using System.Reflection;
 using System.Windows;
 using ReactiveUI;
 using Splat;
 using WindowsOptimizations.Core.GlobalData;
-using WindowsOptimizations.Core.Handlers.Configuration;
 
 namespace WindowsOptimizations.WPF
 {
@@ -14,34 +12,12 @@ namespace WindowsOptimizations.WPF
     /// </summary>
     public partial class App : Application
     {
-        protected override async void OnStartup(StartupEventArgs e)
+        protected override void OnStartup(StartupEventArgs e)
         {
             Directory.CreateDirectory(Paths.Base);
 
-            await ConfigurationHandler.SerializeOnCreationAndDeserialize(Paths.UnnecessaryWindowsServicesJsonFile);
-
             Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetCallingAssembly());
             base.OnStartup(e);
-        }
-
-        protected override void OnExit(ExitEventArgs e)
-        {
-            if (PatchExecutionCheck.HasDisabledUnnecessaryWindowsServices ||
-                PatchExecutionCheck.HasReducedMouseInputLatency ||
-                PatchExecutionCheck.HasOptimizedSystemProfile ||
-                PatchExecutionCheck.HasDebloatedWindows ||
-                PatchExecutionCheck.HasOptimizedNetworkOptions ||
-                PatchExecutionCheck.HasReducedCPUProcesses)
-            {
-                MessageBoxResult result = MessageBox.Show("Some changes require a reboot to take effect. Would you like reboot now?", "Windows Optimizations", MessageBoxButton.YesNo, MessageBoxImage.Information);
-
-                if (result == MessageBoxResult.Yes)
-                {
-                    Process.Start("shutdown", "/r /t 0").Dispose();
-                }
-            }
-
-            base.OnExit(e);
         }
     }
 }
