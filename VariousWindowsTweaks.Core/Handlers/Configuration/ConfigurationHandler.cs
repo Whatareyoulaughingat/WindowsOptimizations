55ba155b63785333465f7377050d41b58ae8aba6
Nikos Naoum <nikosnaoumv2@outlook.com>
2021-08-29 13:38:26 +0300

    Removed static attributes and made the methods void.

--- VariousWindowsTweaks.Core/Handlers/Configuration/ConfigurationHandler.cs
+++ VariousWindowsTweaks.Core/Handlers/Configuration/ConfigurationHandler.cs
@@ -8,27 +8,27 @@ namespace WindowsOptimizations.Core.Handlers.Configuration
     /// <summary>
     /// Handles the way JSON serialization and deserialization works.
     /// </summary>
-    public static class ConfigurationHandler
+    public class ConfigurationHandler
     {
         /// <summary>
         /// Gets or sets a value indicating the instance of <see cref="WindowsServicesData"/>.
         /// </summary>
-        public static WindowsServicesData WindowsServicesDataInstance { get; set; }
+        public WindowsServicesData CurrentWindowsServicesDataInstance { get; set; }
 
         /// <summary>
         /// If the specified file path doesn't exist, creates a new one, serializes it and then deserializes it right after.
         /// </summary>
         /// <param name="jsonFilePath">The path of a JSON file.</param>
         /// <returns>[<see cref="Task"/>] An asynchronous operation.</returns>
-        public static async Task SerializeOnCreationAndDeserialize(string jsonFilePath)
+        public void SerializeOnCreationAndDeserialize(string jsonFilePath)
         {
             if (!File.Exists(jsonFilePath))
             {
-                await using FileStream serializationStream = File.OpenWrite(jsonFilePath);
-                await JsonSerializer.SerializeAsync(serializationStream, new WindowsServicesData(), new JsonSerializerOptions { WriteIndented = true });
+                string jsonData = JsonSerializer.Serialize(new WindowsServicesData(), new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText(jsonFilePath, jsonData);
             }
 
-            await DeserializeAsync(jsonFilePath).ConfigureAwait(false);
+            DeserializeAsync(jsonFilePath);
         }
 
         /// <summary>
@@ -36,10 +36,10 @@ namespace WindowsOptimizations.Core.Handlers.Configuration
         /// </summary>
         /// <param name="jsonFilePath">The path of a JSON file.</param>
         /// <returns>[<see cref="Task"/>] An asynchronous operation.</returns>
-        public static async Task DeserializeAsync(string jsonFilePath)
+        public void DeserializeAsync(string jsonFilePath)
         {
-            await using FileStream deserializationStream = File.OpenRead(jsonFilePath);
-            WindowsServicesDataInstance = await JsonSerializer.DeserializeAsync<WindowsServicesData>(deserializationStream);
+            string jsonData = File.ReadAllText(jsonFilePath);
+            CurrentWindowsServicesDataInstance = JsonSerializer.Deserialize<WindowsServicesData>(jsonData);
         }
     }
 }
