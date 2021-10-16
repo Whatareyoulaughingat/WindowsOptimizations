171e3fbccbc0229de32c813c4264f762ac548d2f
Whatareyoulaughingat <nikosnaoumv1@gmail.com>
2021-10-16 12:08:48 +0300

    New extension for getting and setting up DI easier.

---
+++ WindowsOptimizations.Core/Extensions/LocatorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Splat;
+
+namespace WindowsOptimizations.Core.Extensions
+{
+    public static class LocatorExtensions
+    {
+        public static TService GetAnyService<TService>(this IDependencyResolver resolver, string? contract = null) where TService : class, new()
+        {
+            if (resolver.GetService<TService>() == null)
+            {
+                resolver.RegisterConstant(new TService());
+            }
+
+            return resolver.GetService<TService>();
+        }
+    }
+}
